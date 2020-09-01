7ff63e8a199f1c0e98cd8fcc2c15a6d113af5f49
KonWritesCode <mf.feda@gmail.com>
2020-09-01 13:19:21 +0200

    Test

--- IntroductionScript/IntroductionScript.cpp
+++ IntroductionScript/IntroductionScript.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "    _        _          \n";
-    std::cout << "   |  \\    /  |          \n";
-    std::cout << "   |   \\  /   |         \n";
-    std::cout << "   | |\\ \\/ /| |             \n";
-    std::cout << "   | | \\__/ | |  \n";
-    std::cout << "   | |      | |  \n";
-    std::cout << "   |_|      |_| \n";
+	std::string user_name;
+	std::string userFavoriteColor;
+	int user_age;
+	std::string user_favorite_movie;
+
+	std::cout << "What is your name:\n";
+	std::getline(std::cin, user_name);
+
+	//std::cout << "What is your age:\n";
+	//std::getline(std::cin, user_age);
+
+	std::cout << "What is your favorite color:\n";
+	std::getline(std::cin, userFavoriteColor);
+
+	std::cout << "What is your favorite movie:\n";
+	std::getline(std::cin, user_favorite_movie);
+
+
+	std::cout << "\n---------------------------\n";
+	std::cout << "| Profile                 |\n";
+	std::cout << "|-------------------------|\n";
+	std::cout << "| Name:           | " << user_name << "		|\n";
+//	std::cout << "| Age:            | " << user_age << "	|\n";
+	std::cout << "| Favorite color: | " << userFavoriteColor << "		|\n";
+	std::cout << "| Favorite movie: | " << user_favorite_movie << "		|\n";
+	std::cout << "---------------------------\n";
+
+	return 0;
 }
