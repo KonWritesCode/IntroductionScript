1a5590394009d45c04ebebdfb472632fad266b55
KonWritesCode <mf.feda@gmail.com>
2020-09-01 11:15:34 +0200

    Add project files.

---
+++ IntroductionScript/IntroductionScript.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "                      \n";
+    std::cout << "                      \n";
+    std::cout << "                      \n";
+    std::cout << "                      \n";
+    std::cout << "     _                \n";
+    std::cout << "   .`_`.              \n";
+    std::cout << "   |_H_|       (__)   \n";
+}
